0e233d02b89c65c41f5eb726ed2980a89c016603
Jdwashin9 <Jdwashin9@github.com>
2025-01-25 22:37:12 -0500

    Level 5 and Sound

---
+++ Bubble Pinball/Assets/SharkDialogue.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class SharkDialogue : MonoBehaviour
+
+//{   
+   // public DialogueBoxMover dialogueBoxMover;
+   // public GameObject dialogueBox;
+
+    // Start is called before the first frame update
+   // void Start()
+  //  {
+        
+  //  }
+
+    // Update is called once per frame
+   // void Update()
+   // {
+   //   if (Input.GetKeyDown(KeyCode.RightArrow)) // Move when the Right Arrow key is pressed
+   //     {
+   //         dialogueBoxMover.MoveDialogueBoxToRight(dialogueBox, 50f); // Move by 50 units to the right
+   //     } 
+   // }
+//}
